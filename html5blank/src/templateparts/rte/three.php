e1c5b3b3f27905dcba5f367ddccf0bfb3136b84a
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-19 03:01:27 -0400

    corrected function issue

--- html5blank/src/templateparts/rte/three.php
+++ html5blank/src/templateparts/rte/three.php
@@ -25,7 +25,7 @@
         
 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset  first-sm xs-first">
     
-    <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon2.svg" alt="Green Electricy" />
+    <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon2.svg" alt="Green Electricy" />
     <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-11' ) ) ?>
 
 
