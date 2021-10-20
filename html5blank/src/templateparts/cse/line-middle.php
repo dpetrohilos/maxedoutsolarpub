e2ed280b04b70c605309c530b6ee2c6745e1c1b3
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-20 02:21:52 -0400

    1.20

--- html5blank/src/templateparts/cse/line-middle.php
+++ html5blank/src/templateparts/cse/line-middle.php
@@ -1,7 +1,7 @@
 <div class="wrapper-flull middle-line" id="Service"><div class="wrapper pb8 pt7">
    <section class="row">
     
-       <div class="col-xl-lg-5 col-lg-5 col-md-5 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
+       <div class="col-xl-lg-5 col-lg-5 col-md-10 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
             <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon8.svg" alt="Save $5-$20 dollars a month" />
         
            <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-39' ) ) ?>
@@ -17,7 +17,7 @@
     
         </p>       </div>
        
-       <div class="col-xl-lg-5 col-lg-5 col-md-5 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
+       <div class="col-xl-lg-5 col-lg-5 col-md-10 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
             <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon9.svg" alt="Contractually guaranteed" />
    
                <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-40' ) ) ?>
