f723b037c64b4d5109e27671336a8e3c8271d616
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-20 03:19:15 -0400

    Update guarantee.php

--- html5blank/src/templateparts/guarantee.php
+++ html5blank/src/templateparts/guarantee.php
@@ -2,23 +2,23 @@
     
     <section class="row">
     
-    <div class="col-2 col-offset-1">
+    <div class="col-xs-2">
 <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/tinyicon1.svg" alt="No Switching Suppliers" />    
     </div>
         
-           <div class="col-2">
+           <div class="col-xs-2">
 <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/tinyicon2.svg" alt="No Pannels" />    
     </div>
         
-           <div class="col-2">
+           <div class="col-xs-2">
 <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/tinyicon3.svg" alt="No Cancellation Fees" />    
     </div>
         
-           <div class="col-2">
+           <div class="col-xs-2">
 <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/tinyicon4.svg" alt="No Cost" />    
     </div>
         
-           <div class="col-2">
+           <div class="col-xs-2">
 <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/tinyicon5.svg" alt="Guaranteed Savings" />    
     
     </div>
