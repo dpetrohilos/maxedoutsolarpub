054aee190cb366acde809f9bff292a44093c9367
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-20 02:37:42 -0400

    Update page-splash-material.php

--- html5blank/src/page-splash-material.php
+++ html5blank/src/page-splash-material.php
@@ -1,5 +1,3 @@
 <?php /* Template Name: Savings Rooftop Solar Splash */ get_header(); ?>
-<?php get_template_part( 'templateparts/rts/two' ); ?>
-<?php get_template_part( 'templateparts/rts/line-middle' ); ?>
 
 <?php get_footer('savings'); ?>
