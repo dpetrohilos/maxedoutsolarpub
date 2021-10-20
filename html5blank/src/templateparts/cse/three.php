0bf483dd9a6fad418622c4fb365df12f83e08890
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-20 00:20:56 -0400

    1.12

--- html5blank/src/templateparts/cse/three.php
+++ html5blank/src/templateparts/cse/three.php
@@ -2,19 +2,19 @@
     <div class="wrapper">
     <section class="row pb8">
     
-<div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset first-sm xs-first">
-                <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon1.svg" alt="Solar on someone ele's land" />
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset  first-sm xs-first">
+                <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon1.svg" alt="Solar on someone elses land" />
     <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-34' ) ) ?>
    
 
         </div>
-<div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offsetlast-sm xs-first">
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset  last-sm xs-first">
     
    <p class="btn-wrap">
     
     <!-- Calendly link widget begin -->
 
-<a title="chat" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Talk With Us</a>
+<a title="chat" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Get Signed Up</a>
 <!-- Calendly link widget end -->
     
         </p>
@@ -23,20 +23,20 @@
         
         
         
-<div class="col-lg-4 col-md-4 col-sm-10  col-xs-10 col-xs-offset-1 col-sm-offset first-sm xs-first">
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset  first-sm xs-first">
     
-    <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon12.svg" alt="Good for the planet" />
+    <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon2.svg" alt="Green The Grid" />
     <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-35' ) ) ?>
 
 
         </div>
-<div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset last-sm xs-first">
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset  last-sm xs-first">
     
     <p class="btn-wrap">
     
     <!-- Calendly link widget begin -->
 
-<a title="Green Power" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Talk With Us</a>
+<a title="Green Power" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Sign Up</a>
 <!-- Calendly link widget end -->
     
         </p>
@@ -44,13 +44,13 @@
         </div> 
         
         
-<div class="ccol-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset first-sm xs-first">
-                <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon3.svg" alt="Greens the grid" />
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset 2 first-sm xs-first">
+                <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon12.svg" alt="Power your home with community solar" />
     <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-36' ) ) ?>
   
 
         </div>
-<div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset last-sm xs-first">
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-10 col-xs-offset-1 col-xs-offset last-sm xs-first">
     
    <p class="btn-wrap">
     
