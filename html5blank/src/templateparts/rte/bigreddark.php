6234719b233647f18447b4e4d1bcb439b1e9a894
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-21 16:37:40 -0400

    1.45

--- html5blank/src/templateparts/rte/bigreddark.php
+++ html5blank/src/templateparts/rte/bigreddark.php
@@ -3,9 +3,9 @@
     <section class="row">
         
         <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12">
-        <h2 class="text-center hide-xl hide-lg show-md show-sm show-sm"> No Money Down. Zero upfront costs. 
+        <h2 class="text-center hide-xl hide-lg show-md show-sm show-sm">No Money Down. Zero Upfront Costs.
         </h2>
-        <h2 class="hide-xs hide-sm hide-md show-lg show-xl"> No Money Down. Zero upfront costs. 
+        <h2 class="hide-xs hide-sm hide-md show-lg show-xl"> No Money Down. Zero Upfront Costs.
         </h2>
 
             
