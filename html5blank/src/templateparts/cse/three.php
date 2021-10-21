4b403d06305cfe342f1185227edbb5228127c846
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-21 03:35:54 -0400

    Update three.php

--- html5blank/src/templateparts/cse/three.php
+++ html5blank/src/templateparts/cse/three.php
@@ -23,7 +23,7 @@
         
 <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-xs-offset  first-sm xs-first">
     
-    <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon10.svg" alt="Green The Grid" />
+    <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon15.svg" alt="Green The Grid" />
     <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-35' ) ) ?>
 
 
@@ -31,7 +31,7 @@
 
         
 <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-xs-offset-1 col-xs-offset 2 first-sm xs-first">
-                <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon5.svg" alt="Power your home with community solar" />
+                <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon10.svg" alt="Power your home with community solar" />
     <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-36' ) ) ?>
   
 
