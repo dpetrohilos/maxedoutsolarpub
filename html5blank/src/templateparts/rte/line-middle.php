a75e8e0ba51f76923456c6480ea7cee20bc81f31
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-21 02:46:50 -0400

    Update line-middle.php

--- html5blank/src/templateparts/rte/line-middle.php
+++ html5blank/src/templateparts/rte/line-middle.php
@@ -18,7 +18,7 @@
         </p>       </div>
        
        <div class="col-xl-lg-5 col-lg-5 col-md-5 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
-            <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon8.svg" alt="Green your home" />
+            <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon14.svg" alt="EV car" />
    
                <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-16' ) ) ?>
 
