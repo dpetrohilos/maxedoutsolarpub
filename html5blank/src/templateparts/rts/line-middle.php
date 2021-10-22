86818f2397990c4c2a497b1659d967cb2a175b3e
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-22 08:24:58 -0400

    Update line-middle.php

--- html5blank/src/templateparts/rts/line-middle.php
+++ html5blank/src/templateparts/rts/line-middle.php
@@ -4,7 +4,7 @@
        <div class="col-xl-lg-5 col-lg-5 col-md-5 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
             <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon10.svg" alt="Green Energy" />
         
-           <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-23' ) ) ?>
+    <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-23' ) ) ?>
            
            
            
@@ -20,7 +20,7 @@
        <div class="col-xl-lg-5 col-lg-5 col-md-5 col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
             <img class="img-icon lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Icon12.svg" alt="Clean Environment" />
    
-               <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-24' ) ) ?>
+    <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-24' ) ) ?>
 
             <p class="btn-wrap">
     
