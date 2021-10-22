f9b1a2adc184640613271e1578db559cf8abcb48
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-22 05:19:46 -0400

    1

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -14,7 +14,7 @@
         
         
 
-    <div class="col-lg-5 col-md-5 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 last-sm first-md last-xs middle flex-column">
+    <div class="col-lg-5 col-md-5 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 last-md first-sm last-xs middle flex-column">
         
 <picture>    
 <source class="lazy" sizes="(min-width: 40rem) 80vw, 100vw" 
