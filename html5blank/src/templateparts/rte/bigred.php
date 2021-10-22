909e7b6e516c3d641f98ed57122a64bb6c1ba4d2
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-22 05:42:45 -0400

    1.53

--- html5blank/src/templateparts/rte/bigred.php
+++ html5blank/src/templateparts/rte/bigred.php
@@ -1,9 +1,7 @@
-<div class="wrapper-full pt11 pb8 bgred" id="talk">
-<div class="wrapper">
+<div class="wrapper-full pt11 pb8 bgred bg-solar" id="talk">
     <section class="row">
     
-       
-  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-0 col-md-offset-0 last-sm first-md last-xs">
+    <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1  first-md last-sm last-xs">
 <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
 <script>
   hbspt.forms.create({
@@ -14,7 +12,28 @@
 </script>        
         </div>
         
-           
+        
+
+    <div class="col-lg-5 col-md-5 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 last-md first-sm last-xs">
+        
+<picture>    
+<source class="lazy" sizes="(min-width: 40rem) 80vw, 100vw" 
+ srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam.webp" type="image/webp" />
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam-2@0.75x.jpg" media="(min-width: 1000px)">
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam-1@0.5x.jpg" media="(min-width: 800px)">
+  
+<img width="1200" class="lazy responsive-img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam.jpg" alt="Mother holding child" />
+
+        
+        
+    </picture> 
+ <div class="bgred pt1 mt0 ml0 pl1 pr1 pb1">        <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-9' ) ) ?>
+
+    </div>        
+        
+    </div>
+        
+       
         
     </section>
-    </div></div>
+    </div>
--- html5blank/src/templateparts/rts/bigred.php
+++ html5blank/src/templateparts/rts/bigred.php
@@ -1,8 +1,7 @@
-<div class="wrapper-full pt11 pb8 bgred" id="talk">
-<div class="wrapper">
+<div class="wrapper-full pt11 pb8 bgred bg-solar" id="talk">
     <section class="row">
     
-  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-0 col-md-offset-0 last-sm first-md last-xs">
+    <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1  first-md last-sm last-xs">
 <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
 <script>
   hbspt.forms.create({
@@ -14,26 +13,27 @@
         </div>
         
         
-    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-0 col-md-offset-0 first-sm first-xs last-md mb2">
+
+    <div class="col-lg-5 col-md-5 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 last-md first-sm last-xs">
         
-    <a href="https://www.youtube.com/embed/mAAcHyUKLx0" class="glightbox3"> <!----change after videos are made!---->
-        
-    <picture>    
+<picture>    
 <source class="lazy" sizes="(min-width: 40rem) 80vw, 100vw" 
- srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-3.webp" type="image/video-5.webp" />
-<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-5@0.5x-100.jpg" media="(min-width: 800px)">
-<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-5@0.75x-100.jpg" media="(min-width: 1000px)">
+ srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam.webp" type="image/webp" />
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam-2@0.75x.jpg" media="(min-width: 1000px)">
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam-1@0.5x.jpg" media="(min-width: 800px)">
   
-<img width="624" class="responsive-img radius-both lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-5.jpg" alt="Featured video" />
+<img width="1200" class="lazy responsive-img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam.jpg" alt="Mother holding child" />
 
         
         
-    </picture>
-</a>    
+    </picture> 
+ <div class="bgred pt1 mt0 ml0 pl1 pr1 pb1">        <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-17' ) ) ?>
 
+    </div>        
         
+    </div>
         
-        </div>        
+       
         
     </section>
-    </div></div>
+    </div>
