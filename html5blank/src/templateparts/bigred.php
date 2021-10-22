a9644898e06acd63af28b3d5e19aac598248719b
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-22 04:56:22 -0400

    Update bigred.php

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -1,4 +1,4 @@
-<div class="wrapper-full pt11 pb8 bgred" id="talk">
+<div class="wrapper-full pt11 pb8 bgred bg-solar" id="talk">
 <div class="wrapper">
     <section class="row">
     
@@ -15,7 +15,7 @@
         
         
 
-    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-0 col-md-offset-0 last-sm first-md last-xs bg-solar">
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-0 col-md-offset-0 last-sm first-md last-xs">
         
         
         
