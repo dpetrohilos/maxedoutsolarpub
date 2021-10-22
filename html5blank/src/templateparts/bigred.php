c2cc4c80acd5f3833d1b8d9c94508782feccbbab
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-22 05:04:58 -0400

    Update bigred.php

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -17,7 +17,17 @@
 
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-0 col-md-offset-0 last-sm first-md last-xs">
         
+<picture>    
+<source class="lazy" sizes="(min-width: 40rem) 80vw, 100vw" 
+ srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam.webp" type="image/webp" />
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam-2@0.75x.jpg" media="(min-width: 1000px)">
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam-1@0.5x.jpg" media="(min-width: 800px)">
+  
+<img width="1200" class="lazy responsive-img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/the-fam.jpg" alt="Mother holding child" />
+
         
+        
+    </picture> 
         
         
     </div>
