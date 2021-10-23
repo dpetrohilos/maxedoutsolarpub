91ea31ce49efbabadef7b2abe938b8e52ff1c4cf
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-23 17:30:00 -0400

    1.84

--- html5blank/src/templateparts/rte/bigred.php
+++ html5blank/src/templateparts/rte/bigred.php
@@ -3,7 +3,6 @@
     
     <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1  first-md last-sm last-xs">
         
-        <!--- Move things here ! ---> 
         
         
     
@@ -20,7 +19,7 @@
     </picture> 
         
         
- <div class="bgred pt1 mt0 ml0 pl1 pr1 pb1">        <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-9' ) ) ?>
+ <div class="bgred pt1 mt0 ml0 pl1 pr1 pb1">        <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-17' ) ) ?>
         <p class="btn-wrap">
 
 <a title="chat" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Get a free roof check</a>
@@ -30,14 +29,25 @@
         
         </div></div>
     <div class="col-lg-5 col-md-5 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 last-md first-sm last-xs">
-                <!--- Add Video Here ! ---> 
+<a href="https://www.youtube.com/embed/mAAcHyUKLx0" class="glightbox3"> <!----change after videos are made!---->
+        
+    <picture>    
+<source class="lazy" sizes="(min-width: 40rem) 80vw, 100vw" 
+ srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-3.webp" type="image/video-3.webp" />
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-3.jpg" media="(min-width: 1000px)">
+<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-3@0.75x-100.jpg" media="(min-width: 800px)">
+  
+<img width="624" class="responsive-img radius-both lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-3@0.5x-100.jpg" alt="Featured video" />
 
+        
+        
+    </picture>
+</a>    
 
     </div>        
         
-    </div>
+    
         
        
         
-    </section>
-    </div>
+    </section></div>
