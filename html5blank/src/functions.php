0254abcbd46fe2a8b075c1b38b875063987660db
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-24 17:10:19 -0400

    1.82

--- html5blank/src/functions.php
+++ html5blank/src/functions.php
@@ -841,6 +841,19 @@ function my_init() {
 }
 add_action('init', 'my_init');
  
+// disable styleblock 
+
+// disable gutenberg frontend styles @ https://m0n.co/15
+function disable_gutenberg_wp_enqueue_scripts() {
+	
+	wp_dequeue_style('wp-block-library');
+	wp_dequeue_style('wp-block-library-theme');
+	
+	wp_dequeue_style('wc-block-style'); // disable woocommerce frontend block styles
+	wp_dequeue_style('storefront-gutenberg-blocks'); // disable storefront frontend block styles
+	
+}
+add_filter('wp_enqueue_scripts', 'disable_gutenberg_wp_enqueue_scripts', 100);
 
 
 
--- html5blank/src/header-without.php
+++ html5blank/src/header-without.php
@@ -1,23 +1,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
+	<head><title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
-
+        <meta name="theme-color" content="#f7f6f1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="shortcut icon">
 		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon">
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
-        
-<meta name="theme-color" content="#f7f6f1">
-
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
-
-		<?php wp_head(); ?>
-        
-
-	
+        <?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
 
