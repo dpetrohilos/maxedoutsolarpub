24ea76bf01aba447fe0778bd9e6984d9c1c7c18f
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-24 17:22:12 -0400

    Update bigred.php

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -1,22 +1,6 @@
 <div class="wrapper-full pt10 pb10 bgred bg-solar" id="talk">
     <section class="row">
     
-    <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1  ">
-        
-        
-        
-    
-        
-        
- <div class="bgred pt1 mt0 ml0 pl1 pr1 pb1">        <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
-        <p class="btn-wrap">
-
-<a title="chat" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Get a free roof check</a>
-<!-- Calendly link widget end -->
-    
-        </p>
-        
-        </div></div>
     <div class="col-lg-5 col-md-5 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 ">
 <a href="https://www.youtube.com/embed/mAAcHyUKLx0" class="glightbox3"> <!----change after videos are made!---->
         
@@ -33,7 +17,23 @@
     </picture>
 </a>    
 
-    </div>        
+    </div> 
+    <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1  ">
+        
+        
+        
+    
+        
+        
+ <div class="bgred pt1 mt0 ml0 pl1 pr1 pb1">        <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
+        <p class="btn-wrap">
+
+<a title="chat" class="btn" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Get a free roof check</a>
+<!-- Calendly link widget end -->
+    
+        </p>
+        
+        </div></div>       
         
     
         
