dc9df8aee76b96d759d64abfbc551c8332963df0
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 04:22:46 -0400

    Update guarantee.php

--- html5blank/src/templateparts/guarantee.php
+++ html5blank/src/templateparts/guarantee.php
@@ -1,5 +1,5 @@
 <div class="bg8">
-<div class="wrapper-full big-text core-message" id="vision">
+<div class="wrapper-full big-text" id="vision">
 <section class="row">
     <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-xs-offset-1 col-lg-offset-1">
         <h2 class="text-center">Save $26,000 in 20 years on electrical costs</h2>
