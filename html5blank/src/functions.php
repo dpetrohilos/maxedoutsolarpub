ab48ec336b8ea5cf0b18df5bfd58f4d988492b3c
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 05:49:33 -0400

    Update functions.php

--- html5blank/src/functions.php
+++ html5blank/src/functions.php
@@ -856,4 +856,11 @@ function disable_gutenberg_wp_enqueue_scripts() {
 add_filter('wp_enqueue_scripts', 'disable_gutenberg_wp_enqueue_scripts', 100);
 
 
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
 
