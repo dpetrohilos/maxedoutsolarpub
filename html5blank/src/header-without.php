a9be53362a718737661795cae832de043aa9cfe7
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 08:41:52 -0400

    1.44

--- html5blank/src/header-without.php
+++ html5blank/src/header-without.php
@@ -1,36 +1,17 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
-    	<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5Z3WVDR');</script>
-<!-- End Google Tag Manager -->    
+	<head><title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
-
+        <meta name="theme-color" content="#f7f6f1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="shortcut icon">
 		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon">
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
-        
-<meta name="theme-color" content="#f7f6f1">
-
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
-
-		<?php wp_head(); ?>
-        
-
-
+        <?php wp_head(); ?>
 	</head>
-    
 	<body <?php body_class(); ?>>
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z3WVDR"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
         
 <nav id="mainnav" role="navigation" class="nav hamburger headroom headroom header--fixed hide-from-print animated slideDown">
