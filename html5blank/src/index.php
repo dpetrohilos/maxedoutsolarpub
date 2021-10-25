aac7eb22790b738a21a74e3962cae4da4c03703c
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 04:07:20 -0400

    1.118

--- html5blank/src/index.php
+++ html5blank/src/index.php
@@ -7,12 +7,11 @@
 <?php get_template_part( 'templateparts/core-message' ); ?>
 <?php get_template_part( 'templateparts/line-middle' ); ?>
 <?php get_template_part( 'templateparts/bigtext' ); ?>
-<div class="bg12">
-<div class="wrapper pt10" role="main" aria-label="Content" id="writing">
+<div class="wrapper writing" role="main" aria-label="Content" id="writing">
 		<section class="row content">
 			<?php get_template_part( 'loop' ); ?>
 		</section>
-    </div></div>
+    </div>
 
 
 <?php get_footer('home'); ?>
