19c282363fa5cdc3376b3aaf262c3c18517a25b7
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 04:21:32 -0400

    1.120

--- html5blank/src/templateparts/core-message.php
+++ html5blank/src/templateparts/core-message.php
@@ -1,4 +1,4 @@
-<div class="wrapper-full big-text core-message" id="vision">
+<div class="wrapper-full big-text" id="vision">
 <section class="row">
     <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-xs-offset-1 col-lg-offset-1">
         <h2 class="text-center">Add $15,000 to your homes re-sale value</h2>
