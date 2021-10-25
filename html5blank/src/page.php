fcd3b7267c6c745807393f24c7155ca8725e54a5
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 15:31:39 -0400

    grumble grumble

--- html5blank/src/page.php
+++ html5blank/src/page.php
@@ -4,13 +4,11 @@
 		<!-- section -->
 		<section class="row content">
             
-        <div class="col-12">    
-			<h1><?php the_title(); ?></h1>
-            </div>
 		<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class('content-inner col-lg-6 col-md-6 col-sm-6 col-xs-10 col-lg-offset-3 col-md-offset-3 col-sm-offset-3 col-xs-offset-1'); ?>>
+			<h1><?php the_title(); ?></h1>
 
 				<?php the_content(); ?>
 
