483f40c7f5b8da042367a2abf244cc94ebfe6eaa
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-25 20:32:34 -0400

    Update page.php

--- html5blank/src/page.php
+++ html5blank/src/page.php
@@ -7,7 +7,7 @@
 		<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class('content-inner col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-3 col-sm-offset-3 col-xs-offset-1'); ?>>
+			<article id="post-<?php the_ID(); ?>" <?php post_class('content-inner col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-3'); ?>>
 			<h1><?php the_title(); ?></h1>
 
 				<?php the_content(); ?>
