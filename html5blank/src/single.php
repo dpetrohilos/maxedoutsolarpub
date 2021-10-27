93964fcc298017f926323026df8b159b4dd50c6d
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-27 17:56:44 -0400

    1.151

--- html5blank/src/single.php
+++ html5blank/src/single.php
@@ -18,7 +18,9 @@
      </p> 
     
     </div></section> </div>
-<div class="wrapper-full featured-wrapper middle-line"><section class="row"><div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-xs-offset-1 col-lg-offset-2 col-md-offset-2 ">
+
+
+<div class="wrapper-full featured-wrapper middle-line"><section class="row"><div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-2 text-center">
 
     			<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists. ?>
 					<?php the_post_thumbnail('radius-both responsive-img'); // Fullsize image for the single post. ?>
