b70045cd739fb482b22faa9fa04602017e793679
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-27 10:23:46 -0400

    Update introduction.php

--- html5blank/src/templateparts/introduction.php
+++ html5blank/src/templateparts/introduction.php
@@ -1,8 +1,7 @@
-<div class="bg8">
 <div class="wrapper-full big-text core-message" id="vision">
 <section class="row">
     <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-xs-offset-1 col-lg-offset-1">
         <h2 class="text-center">Our Process</h2>
       </div>
     </section>
-</div></div>  
+</div>
