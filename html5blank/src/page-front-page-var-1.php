28405518d72a60bf0592434aded4108544a71669
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-28 12:16:27 -0400

    Splash Page Var

---
+++ html5blank/src/page-front-page-var-1.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: Front Page Option 2 */ get_header(); ?>
+<div class="wrapper-full hero-part">
+
+    <section class="row hero-container type-element en-lie-copy">
+        <h1>Your clean energy future.</h1>
+        
+        <p class="text-center">Starts today with zero money upfront.</p>   
+        <p class="btn-wrap text-center">
+    
+    <!-- Calendly link widget begin -->
+
+<a title="Free" class="btn-alpha" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Start The Conversation</a>
+            <!-- Calendly link widget end --> <span>No money up front</span>
+    
+        </p> 
+
+    </section>
+</div>
+<?php get_footer(); ?>
