8a61b30216a41a723ca56793ff6945eaa884cd71
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-28 06:30:17 -0400

    404

--- html5blank/src/404.php
+++ html5blank/src/404.php
@@ -5,12 +5,13 @@
 		<section class="section">
 
 			<!-- article -->
-			<article id="post-404">
+			<article id="post-404" class="col">
+<iframe src="https://giphy.com/embed/BM0i93ul1MRW8EjMHe" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cbc-schittscreek-schitts-creek-BM0i93ul1MRW8EjMHe">via GIPHY</a></p>
 
 				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
+				<p class="btn-wrap">
+					<a class="btn" href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
+				</p>
 
 			</article>
 			<!-- /article -->
