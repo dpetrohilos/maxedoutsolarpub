4c8cbec2616ca8eaf6317ae0d4fbef75e9dbccac
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-28 15:03:13 -0400

    Update page-front-page-var-1.php

--- html5blank/src/page-front-page-var-1.php
+++ html5blank/src/page-front-page-var-1.php
@@ -8,7 +8,7 @@
         
     </div>    
     <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-2">    
-        <p>Starts today with zero money upfront. Solar adds about $15,000 in equity to your home’s value, and average rooftop solar customers save $26,000 on electricity costs over 20 years.</p>   
+        <p class="top-text">Starts today with zero money upfront. Solar adds about $15,000 in equity to your home’s value, and average rooftop solar customers save $26,000 on electricity costs over 20 years.</p>   
         <p class="btn-wrap text-center">
     
     <!-- Calendly link widget begin -->
