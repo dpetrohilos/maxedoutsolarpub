ed9db641480123fe2bb8cced3499e22d820b01d3
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-28 14:10:25 -0400

    Update page-front-page-var-1.php

--- html5blank/src/page-front-page-var-1.php
+++ html5blank/src/page-front-page-var-1.php
@@ -1,7 +1,9 @@
 <?php /* Template Name: Front Page Option 2 */ get_header(); ?>
 <div class="wrapper-full hero-part">
-
-    <section class="row"><div class="hero-container col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center around center">
+    
+    <div class="wrapper">
+    <section class="row">
+    <div class="hero-container col-lg-12 col-md-12 col-sm-12 col-xs-12 around">
         <h1>Your clean <br><span class="hero-second">energy future.</span></h1>
         
         <p class="text-center">Starts today with zero money upfront.</p>   
@@ -14,5 +16,5 @@
         </p> 
         </div>
     </section>
-</div>
+    </div></div>
 <?php get_footer('eco'); ?>
