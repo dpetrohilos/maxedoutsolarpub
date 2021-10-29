2dff2449092de167b58c33e81065ea158fa5f515
dpetrohilos <dylanpetrohilos@gmail.com>
2021-10-29 14:49:20 -0400

    1.180

---
+++ html5blank/src/page-front-page-var-2.php
@@ -0,0 +1,145 @@
+<?php /* Template Name: Front Page Option 3 */ get_header(); ?>
+<div class="wrapper-full hero-part">
+    
+    <div class="wrapper">
+    <section class="row">
+    <div class="hero-container col-lg-12 col-md-12 col-sm-12 col-xs-12 around">
+        <h1>Your clean <br/><span class="hero-second"> energy future.</span></h1>
+        
+    </div>    
+    <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-2"><p class="top-text">Starts today with zero money upfront. Solar adds about $15,000 in equity to your home’s value, and average rooftop solar customers save $26,000 on electricity costs over 20 years.</p>   
+        <p class="btn-wrap">
+    
+    <!-- Calendly link widget begin -->
+
+<a title="Free" class="btn-alpha" href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/max-neely/free-roof-check'});return false;">Get Started </a>
+            <!-- Calendly link widget end --> <span>Set up a free consultation today</span>
+        </p> 
+        </div>
+    </section>
+    </div></div>
+
+<div class="wrapper divided">
+    
+<section class="row content">
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">    
+
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/chart1.svg" alt="KPH solar vs. fossil fuels">
+</div>
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">    
+    <h2>Solar means you spend less</h2>
+    <p>The price of solar KPH is over half the price of dirty fossil fuels. Stop paying for pollution and save money and the planet.</p>
+</div>    
+
+    
+</section>
+    
+<div class="wrapper divided">
+    
+<section class="row content">
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"> 
+<h2>Professional rooftop solar panel installation significantly increases your home’s value
+    </h2>    
+    <p>Don’t cut corners and try to DIY. Solar panel installation should be done by qualified experts, like any other home improvement project. In addition to lowering your energy bills, rooftop solar panels will add $15,000 to your home's value. </p>
+    
+</div>
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">    
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/chart2.svg" alt="Homes with rooftop solar sell for thousands more">
+</div>    
+
+    
+</section>
+    
+<div class="wrapper divided">
+    
+<section class="row content">
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">    
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/chart3.svg" alt="Average price of installs have gone down over 11 years">
+</div>
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    <h2>Solar install prices are half what they used to be </h2>
+
+    <p>In 2010, the average price of rooftop solar panel installation was $40,000. By 2021, this figure has dropped to around $20,000. Hiring professional solar installation experts to install solar panels on your roof has never been more affordable.</p>
+    
+</div>    
+
+    
+</section>
+
+<div class="wrapper divided">
+    
+<section class="row content">
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">    
+    <h2>You’re already spending thousands of dollars annually on energy costs </h2>
+<p>
+Oftentimes, one thing we hear from people interested in solar is concern over spending too much money. Solar panels can reduce your energy bills by up to 90% annually. Our average rooftop solar customers save $26,000 over 20 years.
+    </p>
+    
+</div>
+    
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/chart4.svg" alt="Save $26,000 in 20 years on electrical costs">    
+</div>    
+
+    
+</section>
+    
+<div class="wrapper faq">
+    
+<section class="row content">
+    
+<div class="col-xl-8 col-lg-8 col-md-10 col-sm-12 col-xs-12 col-xl-offset-2 col-lg-ofsset-2 col-md-offset-1">
+<dt class="description-title">How Much Does It Cost To Get Solar Panels Installed?
+ <span class="expand-collapse">&#10133;</span></dt>
+            <dd class="description">
+            <p> Every home’s roof is different. Some people work from home and run screens all day, some people have an electric vehicle to charge. Some people take extra care to conserve electricity! Your free quote will reflect your home’s unique needs.
+
+</p></dd>
+    
+    <dt class="description-title">How Long Does It Take To Get Solar Panels Installed? <span class="expand-collapse">&#10133;</span></dt>
+            <dd class="description">
+            <p> As a general rule, between 1-3 months.* Of course, we want to move fast, but we don’t want to be pushy. It’s important to us to do a good job and make sure we get everything right the first time.
+
+*ongoing supply chain disruptions in the Pacific could negatively impact install time.
+
+</p></dd>
+    
+    <dt class="description-title">Do You Offer An Extended Warranty To Cover My Investment?
+ <span class="expand-collapse">&#10133;</span></dt>
+            <dd class="description"><p>One of the best available! Our extended warranty lasts for 30 years and covers solar panels, inverters, mounting systems, all labor costs, and includes gap coverage. The warranty is transferable to new homeowners in the event you sell your home.
+
+</p></dd>
+    
+    <dt class="description-title">What Is Net Metering And How Does Net Metering Work?
+ <span class="expand-collapse">&#10133;</span></dt>
+            <dd class="description"><p>Net metering is a policy that credits you for every kilowatt-hour your system overproduces and puts extra power back into the grid. When your system overproduces, you can bank extra credits to use on a cloudy day, or at night.
+
+</p></dd>    
+
+</div>
+    
+
+    
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer('eco'); ?>
