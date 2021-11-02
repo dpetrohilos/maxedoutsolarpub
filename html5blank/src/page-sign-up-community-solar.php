052de5959d92c8dc4495104bf41878ecf20d856a
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-01 23:17:13 -0400

    Update page-sign-up-community-solar.php

--- html5blank/src/page-sign-up-community-solar.php
+++ html5blank/src/page-sign-up-community-solar.php
@@ -10,7 +10,7 @@
 <article class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-lg-offset-8 col-md-offset-8">
                 
 <section class="row">
-<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Maxed Out Solar" class="logo-img">
                 <?php the_content(); ?>
 
