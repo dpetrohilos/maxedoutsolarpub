4351d65372d33ab4c6ac59fbcc691c197a372cca
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-03 13:32:31 -0400

    1.236

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -1,27 +1,28 @@
-<div class="wrapper-full bgred bg-solar" id="talk"><div class="wrapper">
+<div class="wrapper-full hero"><div class="wrapper hero-container">
+    <section class="row">
+        <div class="col-lg-12 col-sm-12 ">
+            <h1>Your Clean <br> <span>Energy Future</span>
+            
+            </h1>
+            
+                  <p class="btn-wrap text-center">
+    
+    <!-- Calendly link widget begin -->
+
+<a title="Free Consultation Link" class="btn-alpha" href="https://maxedoutsolar.com/sign-up/">Get A free Roof Check 
+            </a></p> 
+            
+            </div>
+    </section></div>
+</div>
+
+<div class="wrapper-full bgred bg-solar" id="talk"><div class="wrapper">    
     <section class="row">
     
-    <div class="col-lg-6 col-md-5 col-sm-12 col-xs-12">
-<a href="https://www.youtube.com/embed/_VNx83azM5M" class="glightbox3"> <!----change after videos are made!---->
-        
-    <picture>    
-<source class="lazy" sizes="(min-width: 40rem) 80vw, 100vw" 
- srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-2.webp" type="image/webp" />
-<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-2@0.5x-80.jpg" media="(min-width: 1000px)">
-<source class="lazy" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-2@0.75x-80.jpg" media="(min-width: 800px)">
-  
-<img width="624" class="responsive-img radius-both lazy" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/video-2@1.5x-50.jpg" alt="Featured video" />
-
-        
-        
-    </picture>
-</a>    
-
-    </div> 
-    <div class="col-lg-6 col-md-5 col-sm-12 col-xs-12">
+<div class="col-lg-6 col-md-5 col-sm-12 col-xs-12">
         
       
-               <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
+<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
         <p class="btn-wrap">
     
     <!-- Calendly link widget begin -->
