8ded6b69d3e707d2afacf03a87a886d65ec1c3e4
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-03 16:15:32 -0400

    Update bigred.php

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -3,6 +3,11 @@
         <div class="col-lg-6 col-sm-12"><span class="hero-container">
 <h1>Your Clean <span>Energy Future</span></h1></span>
             
+            
+        </div></section><section class="row">
+    
+    <div class="col-lg-6 col-sm-12">
+            
             <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
                   <p class="btn-wrap">
     
@@ -13,7 +18,7 @@
             
             </div>
             
-    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 last-lg last-xl last-md first-sm first-xs">
+    <div class="col-lg-6 col-sm-12">
                   
 
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/rtsolar-illustration.svg" alt="Happy Family In A sustainable Future" width="1280" height="800">
