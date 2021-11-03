6926978908d0ba96c69373059ebd52df9c5f0af5
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-03 16:12:09 -0400

    2.253

--- html5blank/src/templateparts/bigred.php
+++ html5blank/src/templateparts/bigred.php
@@ -1,13 +1,8 @@
 <div class="wrapper-full hero bg-solar"><div class="wrapper">
     <section class="row">
-        <div class="col-lg-12 col-sm-12 hero-container">
-<h1>Your Clean <br> <span>Energy Future</span>
+        <div class="col-lg-6 col-sm-12 hero-container">
+<h1>Your Clean <span>Energy Future</span>
             </h1>
-            
-            </div>
-    </section>
-        <section clas="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 first-lg first-xl first-md last-sm last-xs">
             <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
                   <p class="btn-wrap">
     
@@ -19,9 +14,10 @@
             </div>
             
     <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 last-lg last-xl last-md first-sm first-xs">
-                   
+                  
 
-            
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/rtsolar-illustration.svg" alt="Happy Family In A sustainable Future" width="640" height="400">
+
         </div>
             
     </section></div>
