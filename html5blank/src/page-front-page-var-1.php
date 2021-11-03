e3b515e7236110615d2a4082b0e2ab724264206f
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-03 13:15:34 -0400

    Fixes

--- html5blank/src/page-front-page-var-1.php
+++ html5blank/src/page-front-page-var-1.php
@@ -3,17 +3,13 @@
     
     <div class="wrapper">
     <section class="row">
-    <div class="hero-container col-lg-12 col-md-12 col-sm-12 col-xs-12 around">
-        <h1>Your clean <br/><span class="hero-second"> energy future.</span></h1>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1>Your clean energy future</h1>
         
-    </div>    
-    <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-2"><p class="top-text">Starts today with zero money upfront. Solar adds about $15,000 in equity to your home’s value, and average rooftop solar customers save $26,000 on electricity costs over 20 years.</p>   
-                <p class="btn-wrap">
-    
-    <!-- Calendly link widget begin -->
-
-<a title="Free Consultation Link" class="btn-alpha" href="https://maxedoutsolar.com/sign-up/">Get Started </a>
-            <!-- Calendly link widget end --> <span>Set up a free consultation today</span>
+        <p class="top-text">Starts today with zero money upfront. Solar adds about $15,000 in equity to your home’s value, and average rooftop solar customers save $26,000 on electricity costs over 20 years.</p>   
+        <p class="btn-wrap">
+        <a title="Free Consultation Link" class="btn-alpha" href="https://maxedoutsolar.com/sign-up/">Get Started </a>
+        <span>Set up a free consultation today</span>
         </p> 
         </div>
     </section>
