28b8df0c60e93cc5315ca73ab1be3dc2acc6e236
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-05 16:34:20 -0400

    Update index.php

--- html5blank/src/index.php
+++ html5blank/src/index.php
@@ -18,11 +18,10 @@
     </picture>
 </a>    
 
-    
     <p class="text-center">
     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Maxed Out Solar" class="logo-img text-center">
     </p>
-    <h2>Your Green Future. Your Green Energy Community</h2>
+    <h2>Your Green Future Starts Today</h2>
     
     <p>Our cooperative was founded in 2020 during the Great Resignation and the Covid-19 pandemic.</p>
     
