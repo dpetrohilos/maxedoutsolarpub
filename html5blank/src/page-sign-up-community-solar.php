f0df06af82aae9b49818cb3a3395dcead534660a
dpetrohilos <dylanpetrohilos@gmail.com>
2021-11-05 13:08:55 -0400

    Update page-sign-up-community-solar.php

--- html5blank/src/page-sign-up-community-solar.php
+++ html5blank/src/page-sign-up-community-solar.php
@@ -1,18 +1,16 @@
 <?php /* Template Name: Signup Community Solar */ get_header('no-nav'); ?>
-    <?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
+<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-        
-
-<div class="wrapper-fluid sign-up-pages" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;">
-        <section class="row">
+<div class="wrapper-fluid sign-up-pages" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;"> <section class="row">
             
 <article class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-lg-offset-8 col-md-offset-8">
                 
 <section class="row">
 <div class="col-xl-6 col-lg-9 col-md-12 col-sm-12 col-xs-12">
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Maxed Out Solar" class="logo-img">
-                <?php the_content(); ?>
+
+<?php the_content(); ?>
 
 <div data-paperform-id="find-my-local-community-solar-project" bg_id="maxedoutsolar" bgc_id="solar-with-no-panels" bg_pid="swnp-opt-in"></div><script>(function() {var script = document.createElement('script'); script.src = "https://paperform.co/__embed.min.js"; document.body.appendChild(script); })()</script>
     
