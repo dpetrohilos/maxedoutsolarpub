8274d83d8ad1d584d29292c708a011c2e778c743
dpetrohilos <dylanpetrohilos@gmail.com>
2021-12-08 01:29:43 -0500

    huh odd

--- html5blank/src/page-buyer.php
+++ html5blank/src/page-buyer.php
@@ -12,8 +12,7 @@
                 
 <section class="row">
 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Maxed Out Solar" class="logo-img">
-<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/timeline1.svg" alt="Step 1">
+
     
                            <h1><?php the_title(); ?></h1>
                 <?php the_content(); ?>
